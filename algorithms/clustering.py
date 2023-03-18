cdaa3d417a8ef66ea89897af33d0d3a260be560b
Fedor Kabachenko <64920367+padobrik@users.noreply.github.com>
2023-03-18 06:14:09 -0500

    Added model training

--- algorithms/clustering.py
+++ algorithms/clustering.py
@@ -36,50 +36,52 @@ class UNet(nn.Module):
         x3 = self.decoder(x2)
         return x3
 
-def preprocess_image(image_path):
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
-    image = image.astype(np.float32) / 255.0
-    image = np.expand_dims(image, axis=0)
-    image = np.expand_dims(image, axis=0)
-    return torch.tensor(image)
-
-def postprocess_prediction(prediction):
-    prediction = prediction.detach().numpy()
-    prediction = np.argmax(prediction, axis=1)
-    prediction = np.squeeze(prediction, axis=0).astype(np.uint8)
-    return prediction
-
 if __name__ == "__main__":
-    # Load the image
-    image_path = "path/to/your/image.png"
-    image_tensor = preprocess_image(image_path)
+    # Create an instance of the U-Net model
+    model = UNet(in_channels=1, out_channels=1)
+    model = model.to('cuda')  # Move the model to GPU if available
 
-    # Create a U-Net model
-    model = UNet(in_channels=1, out_channels=2)
+    # Define a loss function and an optimizer
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    # Load the pre-trained model weights
-    model_weights_path = "path/to/your/model_weights.pth"
-    if os.path.exists(model_weights_path):
-        model.load_state_dict(torch.load(model_weights_path))
-    else:
-        print("Error: Model weights not found. Train the model and save the weights.")
+    # Create a DataLoader for efficient batching
+    batch_size = 4
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-    # Perform contour detection
-    with torch.no_grad():
-        output = model(image_tensor)
+    # Train the model
+    num_epochs = 50
 
-    # Post-process the output
-    prediction = postprocess_prediction(output)
+    for epoch in range(num_epochs):
+        print("Epoch {}/{}".format(epoch + 1, num_epochs))
 
-    # Display the results
-    original_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    resized_image = cv2.resize(original_image, (256, 256), interpolation=cv2.INTER_AREA)
-    cv2.imshow("Original Image", resized_image)
-    cv2.imshow("Contour Detection", prediction * 255)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        model.train()  # Set the model to training mode
+        running_loss = 0.0
 
-    # Save the output
-    output_path = "path/to/save/output.png"
-    cv2.imwrite(output_path, prediction * 255)
+        for batch_idx, (images, masks) in enumerate(dataloader):
+            images = images.to('cuda')  # Move the images to GPU if available
+            masks = masks.to('cuda')  # Move the masks to GPU if available
+
+            # Zero the gradients
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(images)
+
+            # Calculate the loss
+            loss = criterion(outputs, masks)
+
+            # Backward pass
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        # Calculate and print the average loss for this epoch
+        epoch_loss = running_loss / len(dataloader)
+        print("Loss: {:.4f}".format(epoch_loss))
+        
+    # Save the trained model
+    model_path = "trained_unet.pth"
+    torch.save(model.state_dict(), model_path)
+    
