8154343ea8959b87b627b94da41adc552e892022
Fedor Kabachenko <64920367+padobrik@users.noreply.github.com>
2023-03-26 14:50:21 +0300

    Added ImageAligner

--- processing.py
+++ processing.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-from typing import Union
+from typing import Union, List, Tuple
 from PIL import Image
 # from sklearn.preprocessing import normalize
 
@@ -12,10 +12,15 @@ class SpotDetector:
         self.current_path = os.getcwd()
         self.files_path = f'{self.current_path}/files'
 
-    def _read_images(self, input_folder: str) -> np.ndarray:
+    def _read_images(self, input_folder: str) -> List[np.ndarray]:
         """
-        Reads image according to the provided paths in __init__(),
-        Returns grayscale version using CV2 lib
+        Read the images from the specified path.
+
+        Args:
+            path (str): The path to the folder containing the images.
+
+        Returns:
+            List[np.ndarray]: A list of images as NumPy arrays.
         """
         images = []
         for filename in os.listdir(input_folder):
@@ -108,13 +113,92 @@ class SpotDetector:
 class ImageProcessor:
     def __init__(self, sd: SpotDetector) -> None:
         self.sd = sd
-    
-    def save_images(self, images, output_folder) -> None:
+
+    def process_image(self, image: np.ndarray) -> None:
+        """
+        Process an image using the SpotDetector instance.
+
+        Args:
+            image (np.ndarray): The input image as a NumPy array.
+        """
+        with_clahe = self.sd._clahe(image, 2.5, (1,1))
+        with_threshold = self.sd._threshold(with_clahe, 0, 255)
+        adaptive = self.sd._adaptive_threshold(with_clahe, 255, 75, 5)
+        blob = self.sd._apply_morphology(adaptive)
+        contours = self.sd._get_contours(blob)
+        self.sd._draw_contours(image, contours)
+
+
+class ImageAligner:
+    def __init__(self, sd: SpotDetector, ip: ImageProcessor) -> None:
+        self.sd = sd
+        self.ip = ip
+
+    def align_images(self, images: List[np.ndarray]) -> List[np.ndarray]:
+        """
+        Align a list of images using the DIHE method.
+
+        Args:
+            images (List[np.ndarray]): A list of images as NumPy arrays.
+
+        Returns:
+            List[np.ndarray]: A list of aligned images as NumPy arrays.
+        """
+        aligned_images = []
+        reference_image = images[0]
+
+        # Pass the first image through the DIHE network
+        dihe = cv2.createAlignMTB()
+        dihe.process([images[0]], [])
+        aligned_images.append(images[0])
+
+        for img in images[1:]:
+            # Resize the image to match the reference image
+            img = cv2.resize(img, (reference_image.shape[1], reference_image.shape[0]), interpolation=cv2.INTER_CUBIC)
+
+            # Convert the image to the format accepted by DIHE
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
+            img = np.uint8(img)
+
+            # Pass the current image through the DIHE network
+            aligned_image = np.zeros_like(img)
+            dihe.process([img], aligned_image)
+
+            aligned_images.append(aligned_image)
+
+        return aligned_images
+
+    def process_images(self, input_folder: str) -> None:
+        images = self.sd._read_images(input_folder)
+        if len(images) > 0:
+            aligned_images = self.align_images(images)
+            for idx, image in enumerate(aligned_images):
+                print(f"Processing image {idx + 1}/{len(aligned_images)}")
+                self.ip.process_image(image)
+        else:
+            print("No images found.")
+            
+    def save_images(self, images: List[np.ndarray], output_folder: str) -> None:
+        """
+        Save the images to the specified output folder.
+
+        Args:
+            images (List[np.ndarray]): A list of images as NumPy arrays.
+            output_folder (str): The path to the folder where the images should be saved.
+        """
         os.makedirs(output_folder, exist_ok=True)
         for i, img in enumerate(images):
             cv2.imwrite(os.path.join(output_folder, f'aligned_image_{i}.tif'), img)
 
     def process_images(self, input_folder: str, output_folder: str) -> None:
+        """
+        Process the images in the input folder and save the aligned images to the output folder.
+
+        Args:
+            input_folder (str): The path to the folder containing the input images.
+            output_folder (str): The path to the folder where the aligned images should be saved.
+        """
         images = self.sd._read_images(input_folder)
         if len(images) > 0:
             aligned_images = self.align_images(images)
@@ -125,7 +209,14 @@ class ImageProcessor:
         else:
             print("No images found.")
 
-    def display_image_pairs(self, raw_folder: str, aligned_folder: str):
+    def display_image_pairs(self, raw_folder: str, aligned_folder: str) -> None:
+        """
+        Display pairs of raw and aligned images side by side.
+
+        Args:
+            raw_folder (str): The path to the folder containing the raw images.
+            aligned_folder (str): The path to the folder containing the aligned images.
+        """
         raw_images = self.sd._read_images(raw_folder)
         aligned_images = self.sd._read_images(aligned_folder)
 
@@ -147,22 +238,8 @@ class ImageProcessor:
 
         plt.tight_layout()
         plt.show()
-    def process_image(self, image: np.ndarray) -> None:
-        """
-        Processes a single image using the SpotDetector methods to apply
-        CLAHE, threshold, adaptive threshold, morphology, and contour detection.
-        Draws the detected contours on the image and displays the result.
-        
-        Args:
-            image: A numpy array representing the input image.
-        """
-        with_clahe = self.sd._clahe(image, 2.5, (1,1))
-        with_threshold = self.sd._threshold(with_clahe, 0, 255)
-        adaptive = self.sd._adaptive_threshold(with_clahe, 255, 75, 5)
-        blob = self.sd._apply_morphology(adaptive)
-        contours = self.sd._get_contours(blob)
-        self.sd._draw_contours(image, contours)
 
+        
 if __name__ == "__main__":
     sd = SpotDetector()
     ip = ImageProcessor(sd)
